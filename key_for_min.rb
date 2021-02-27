8f3fe9280e140d9b561bf2992935db244174963a
Jessica Antunes <jessantunes@protonmail.com>
2021-02-27 15:32:30 -0500

    commit tree a5581a7f846a4f23555854de891becb700b8d76f

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+    least_value = 600
+    key_name = nil   
+    name_hash.each_pair do |key, value|
+        if  least_value >= name_hash[key]
+            least_value = name_hash[key]  
+            key_name = key
+        end
+    end
+    key_name
+end                                        
